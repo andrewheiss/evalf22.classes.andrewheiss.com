77b982690eee3c783f86f49b91e91a992712636c
Andrew Heiss <andrew@andrewheiss.com>
2022-07-27 00:25:48 -0400

    MAGICAL use targets to save CSVs, zip files, and build the site!!!

---
+++ R/tar_save-files.R
@@ -0,0 +1,27 @@
+# When using a file-based target, {targets} requires that the function that
+# saves the file returns a path to the file. write_csv() invisibly returns the
+# data frame being written, so we need a wrapper function to save the file and
+# return the path.
+save_csv <- function(df, path) {
+  readr::write_csv(df, path)
+  return(path)
+}
+
+
+# Zip a folder inside a parent folder
+#
+# The cd is necessary because zip assumes that it's working in the current
+# folder. That is, if you do `zip -r projects/whatever.zip projects/whatever/`,
+# you'll end up with a folder named "projects" in the resulting .zip file, which
+# is annoying. Moving into "projects" first *and then* zipping from there is the
+# only way to fix this.
+#
+# Also annoying is how system2() works. The first argument (command) must be a
+# regular bash command (here `cd`). There's no intuitive way to concatenate
+# commands like `cd blah; zip bloop`, so we feed the second command (here `zip`)
+# as an argument to `cd`, which feels hacky, but it works :shrug:
+zippy <- function(folder_to_zip, parent) {
+  system2("cd", c(parent, "; zip", "-FSrX", paste0(folder_to_zip, ".zip"),
+                  folder_to_zip, '-x "*.DS_Store"'))
+  return(file.path(parent, paste0(folder_to_zip, ".zip")))
+}
---
+++ _targets.R
@@ -0,0 +1,58 @@
+library(targets)
+library(tarchetypes)
+suppressPackageStartupMessages(library(dplyr))
+
+options(tidyverse.quiet = TRUE,
+        dplyr.summarise.inform = FALSE)
+
+tar_option_set(
+  packages = c("tibble"),
+  format = "rds",
+  workspace_on_error = TRUE
+)
+
+# here::here() returns an absolute path, which then gets stored in tar_meta and
+# becomes computer-specific (i.e. /Users/andrew/Research/blah/thing.Rmd).
+# There's no way to get a relative path directly out of here::here(), but
+# fs::path_rel() works fine with it (see
+# https://github.com/r-lib/here/issues/36#issuecomment-530894167)
+here_rel <- function(...) {fs::path_rel(here::here(...))}
+
+# Load functions for the pipeline
+source("R/tar_save-files.R")
+
+# THE PIPELINE ----
+list(
+  ## Knit xaringan slides ----
+  ## Create project folders and zip files ----
+  ### Save any data files from packages ----
+  tar_target(data_penguins,
+             save_csv(tidyr::drop_na(palmerpenguins::penguins, body_mass_g),
+                      here_rel("projects", "problem-set-2", "data", "penguins.csv"))),
+  ### Link all these data files into one dependency
+  tar_target(build_data, {
+    data_penguins
+  }),
+
+  ### Dynamic branching! ----
+  #
+  # Get a list of all folders in the project folder, create dynamic branches,
+  # then create a target for each that runs the custom zippy() function, which
+  # uses system2() to zip the folder and returns a path to keep targets happy
+  # with `format = "file"`
+  #
+  # The main index.qmd page loads project_zips as a target to link it as a dependency
+  tar_target(project_paths, list.dirs(here_rel("projects"), full.names = FALSE, recursive = FALSE)),
+  tar_target(project_files, project_paths, pattern = map(project_paths)),
+  tar_target(project_zips, {
+    build_data
+    zippy(project_files, "projects")
+  },
+  pattern = map(project_files),
+  format = "file"),
+
+  ## Build site ----
+  tar_quarto(site, path = ".")
+
+  ## Upload site ----
+)
