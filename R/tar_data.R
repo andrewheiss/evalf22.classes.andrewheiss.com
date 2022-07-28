2a215aeb53ce373d6a7681049fd9afe0aae347bf
Andrew Heiss <andrew@andrewheiss.com>
2022-07-28 14:54:47 -0400

    Generate mosquito net and village data

--- R/tar_data.R
+++ R/tar_data.R
@@ -67,6 +67,24 @@ save_data <- list(
              format = "file"),
 
   ### Generate and save fake data ----
+  # Mosquito net data
+  tar_target(gen_nets, create_nets()),
+  tar_target(data_nets,
+             save_csv(gen_nets,
+                      here_rel("files", "data", "generated_data", "mosquito_nets.csv")),
+             format = "file"),
+
+  # Village RCT data
+  tar_target(gen_village, create_village()),
+  tar_target(data_village_rct,
+             save_csv(gen_village$village_randomized,
+                      here_rel("files", "data", "generated_data", "village_randomized.csv")),
+             format = "file"),
+  tar_target(data_village_obs,
+             save_csv(gen_village$village_self_selected,
+                      here_rel("files", "data", "generated_data", "village_observational.csv")),
+             format = "file"),
+
   # Fake rain barrel RCT and observational data for problem set 3
   # DAG plots
   tar_target(gen_barrel_dags, create_barrel_dags()),
@@ -93,7 +111,6 @@ save_data <- list(
              save_csv(gen_barrels$rain,
                       here_rel("files", "data", "generated_data", "barrels_observational.csv")),
              format = "file"),
-
 
   # Fake attendance program for regression discontinuity
   tar_target(gen_attendance, create_attendance()),
@@ -181,6 +198,112 @@ save_data <- list(
 
 
 # Data creation functions ----
+
+create_village <- function() {
+  set.seed(1234)
+
+  village <- tibble(id = 1:1000) %>%
+    mutate(sex = rbinom(n(), 1, 0.6),
+           age = rnorm(n(), mean = 35, sd = 10),
+           pre_income = rnorm(n(), mean = 800, sd = 100))
+
+  village_self_selected <- village %>%
+    mutate(prob_program = (15 * sex) + (1.5 * age) + (0.5 * pre_income / 100),
+           prob_program = rescale(prob_program, to = c(0.5, 0.95))) %>%
+    mutate(program = rbinom(n(), 1, prob_program)) %>%
+    mutate(post_income = 800 + (20 * sex) + (10 * age) + (0.5 * pre_income / 100) +
+             (100 * program) + rnorm(n(), 15, 5)) %>%
+    select(-prob_program) %>%
+    mutate(across(c(age, pre_income, post_income), ~round(., 0)))
+
+  village_randomized <- village %>%
+    mutate(program = rbinom(n(), 1, 0.5)) %>%
+    mutate(post_income = 800 + (20 * sex) + (10 * age) + (0.5 * pre_income / 100) +
+             (100 * program) + rnorm(n(), 15, 5)) %>%
+    mutate(sex_num = sex, program_num = program) %>%
+    mutate(across(c(age, pre_income, post_income), ~round(., 0))) %>%
+    mutate(sex = factor(sex_num, labels = c("Female", "Male")),
+           program = factor(program_num, labels = c("No program", "Program")))
+
+  return(lst(village_self_selected, village_randomized))
+}
+
+
+create_nets <- function() {
+  num <- 1752
+
+  # Create confounder variables that are related to each other
+  mu <- c(income = 900, temperature = 75, health = 50)
+  stddev <- c(income = 200, temperature = 10, health = 20)
+  lower <- c(income = 100, temperature = 60, health = 5)
+  upper <- c(income = 2000, temperature = 90, health = 100)
+
+  # https://stackoverflow.com/a/46563034/120898
+  correlations_confounders <- tribble(
+    ~var1, ~var2, ~correlation,
+    "income", "temperature", 0.2,
+    "income", "health", 0.8,
+    # "temperature", "health", 0.6,
+    "temperature", "health", 0.2,
+  ) %>%
+    mutate_at(vars(starts_with("var")),
+              ~factor(., levels = c("income", "temperature", "health"))) %>%
+    xtabs(correlation ~ var1 + var2, ., drop.unused.levels = FALSE) %>%
+    '+'(., t(.)) %>%
+    `diag<-`(1) %>%
+    as.data.frame.matrix() %>% as.matrix()
+
+  # Convert correlation matrix to covariance matrix using fancy math
+  cov_matrix_confounders <- stddev %*% t(stddev) * correlations_confounders
+
+  # Force the covariance matrix to be positive definite and symmetric
+  # https://stats.stackexchange.com/q/153166/3025
+  sigma <- as.matrix(Matrix::nearPD(cov_matrix_confounders)$mat)
+
+  set.seed(123)
+  confounders <- tmvtnorm::rtmvnorm(num, mean = mu, sigma = sigma,
+                                    lower = lower, upper = upper) %>%
+    magrittr::set_colnames(names(mu)) %>% as_tibble() %>%
+    mutate(health = round(health, 0),
+           temperature = round(temperature, 1))
+
+  set.seed(123)
+  mosquito_nets <- tibble(id = 1:num) %>%
+    bind_cols(confounders) %>%
+    mutate(household = rpois(n(), 2) + 1) %>%
+    mutate(enrolled = household > 4 & income < 700) %>%
+    mutate(resistance = rescale(rnorm(n(), 0, 1), to = c(5, 95))) %>%
+    # Simulate data from a logit model: https://stats.stackexchange.com/a/46525/3025
+    # But then do all sorts of weird distortion to change the likelihood of using a net
+    mutate(net_effect = (1.85 * income / 10) + (-1.7 * temperature) + (1.8 * health / 10) +
+             (150 * enrolled) + (2.9 * household),
+           net_diff = net_effect - mean(net_effect),
+           net_effect = ifelse(net_diff < 0, net_effect - (net_diff / 2), net_effect),
+           net_effect_rescaled = rescale(net_effect, to = c(-2.2, 2.2)),
+           inv_logit = 1 / (1 + exp(-net_effect_rescaled)),
+           net_num = rbinom(n(), 1, inv_logit),
+           net = net_num == 1) %>%
+    mutate(malaria_risk_effect = (-5 * income / 10) + (3.9 * temperature) +
+             (1.4 * resistance) + (9 * health / 10) + (-80 * net_num),
+           malaria_risk_diff = malaria_risk_effect - mean(malaria_risk_effect),
+           malaria_risk_effect = ifelse(malaria_risk_diff < 0,
+                                        malaria_risk_effect - (malaria_risk_diff / 2),
+                                        malaria_risk_effect),
+           malaria_risk_effect_rescaled = rescale(malaria_risk_effect, to = c(-2.2, 2.2)),
+           malaria_risk = 1 / (1 + exp(-malaria_risk_effect_rescaled)),
+           malaria_risk = round(malaria_risk * 100, 0)) %>%
+    mutate_at(vars(income, resistance), ~round(., 0)) %>%
+    mutate(temperature = (temperature - 32) * 5/9,
+           temperature = round(temperature, 1)) %>%
+    mutate(malaria_risk = malaria_risk)
+
+  mosquito_nets_final <- mosquito_nets %>%
+    select(id, net, net_num, malaria_risk, income, health, household,
+           eligible = enrolled, temperature, resistance)
+
+  return(mosquito_nets_final)
+}
+
 
 # Rain barrel dags and data for problem set 3
 create_barrel_dags <- function() {
