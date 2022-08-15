fc68e4c05b5414119113580999dbd518e5d54979
Andrew Heiss <andrew@andrewheiss.com>
2022-08-15 13:55:49 -0400

    Fix spacing after slide buttons

--- R/slide-things.R
+++ R/slide-things.R
@@ -1,5 +1,5 @@
 slide_buttons <- function(slide_id) {
-  glue::glue('<p><a class="btn btn-danger" target="_blank" href="{slide_id}.html"><i class="fa-solid fa-arrow-up-right-from-square"></i> View all slides in new window</a> <a class="btn btn-danger" target="_blank" href="{slide_id}.pdf" role="button"><i class="fa-solid fa-file-pdf"></i> Download PDF of all slides</a></p>')
+  glue::glue('<p class="buttons"><a class="btn btn-danger" target="_blank" href="{slide_id}.html"><i class="fa-solid fa-arrow-up-right-from-square"></i> View all slides in new window</a> <a class="btn btn-danger" target="_blank" href="{slide_id}.pdf" role="button"><i class="fa-solid fa-file-pdf"></i> Download PDF of all slides</a></p>')
 }
 
 slide_tabs <- function(slide_df, slide_url) {
